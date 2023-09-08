f52f15c7584cb72a9e18aac434dd3c06bb00dcdb
Ibson Barboza de Oliveira <128850394+IbsonBarboza@users.noreply.github.com>
2023-09-08 13:21:01 -0400

    clock

---
+++ treino-com-relogio-simples/js/script.js
@@ -0,0 +1,30 @@
+const hourHand = document.querySelector('.hour-hand')
+const minuteHand = document.querySelector('.minute-hand')
+const secndHand = document.querySelector('.second-hand')
+
+const getTime = () => {
+    const date = new Date()
+    
+    return {
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+    }
+    
+    /*const obj = {
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+    }
+
+    return obj*/
+}
+
+setInterval(() => {
+    const { seconds , minutes , hours } = getTime()
+
+    secndHand.style.transform = `translate(0, -50%) rotate(${seconds*6}deg)`;
+    minuteHand.style.transform = `translate(0, -50%) rotate(${minutes*6}deg)`
+    hourHand.style.transform = `translate(0, -50%) rotate(${hours*30}deg)`
+
+}, 1000)
