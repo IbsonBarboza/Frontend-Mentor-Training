0f9383e533bc7e7c5d31a0802671617a30545636
Ibson Barboza de Oliveira <128850394+IbsonBarboza@users.noreply.github.com>
2023-10-14 23:20:50 -0400

    interative-rating-update

--- interactive-rating/script.js
+++ interactive-rating/script.js
@@ -3,15 +3,15 @@ const rating = document.querySelector('.rating_choice')
 const submit = document.querySelector('.submit')
 
 
-//Trocar de Pagina
+//Change Page
 function pag2click() {
     window.location.href = './pags/pag2.html'   
 }
 
-//Indentificar Botão
+//Identify Button
 function identifyClickedButton(event) {
     const buttonClick = event.target.textContent;
-    //Armazena o botão que foi clicado
+    //Stores the button that was clicked
     localStorage.setItem("buttonClick", buttonClick);
 }
 
@@ -19,7 +19,7 @@ circles.forEach(function(circles) {
     circles.addEventListener("click", identifyClickedButton);
 })
 
-//Recupera o botão clicado que foi armazenado
+//Retrieves the clicked button that was stored
 const buttonClick = localStorage.getItem("buttonClick")
     if (buttonClick) {
         rating.innerHTML = `You selected ${buttonClick} out of 5`
