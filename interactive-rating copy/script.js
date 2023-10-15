6e64331ade2b62d8c5f10d46c798d2a7a4e4545f
Ibson Barboza de Oliveira <128850394+IbsonBarboza@users.noreply.github.com>
2023-10-15 00:17:56 -0400

    pdp

---
+++ interactive-rating copy/script.js
@@ -0,0 +1,27 @@
+const circles = document.querySelectorAll('.rating_circle')
+const rating = document.querySelector('.rating_choice')
+const submit = document.querySelector('.submit')
+const hide = document.querySelector('.hide')
+
+
+//Change Page
+function pag2click() {
+    hide.style.display = `flex`
+}
+
+//Identify Button
+function identifyClickedButton(event) {
+    const buttonClick = event.target.textContent;
+    //Stores the button that was clicked
+    localStorage.setItem("buttonClick", buttonClick);
+}
+
+circles.forEach(function(circles) {
+    circles.addEventListener("click", identifyClickedButton);
+})
+
+//Retrieves the clicked button that was stored
+const buttonClick = localStorage.getItem("buttonClick")
+    if (buttonClick) {
+        rating.innerHTML = `You selected ${buttonClick} out of 5`
+    }
