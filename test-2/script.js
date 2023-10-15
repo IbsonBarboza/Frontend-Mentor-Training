e70fbcb97ce3deafff9473a3bcb08afff7f79cb4
Ibson Barboza de Oliveira <128850394+IbsonBarboza@users.noreply.github.com>
2023-10-15 00:53:06 -0400

    pa

---
+++ test-2/script.js
@@ -0,0 +1,31 @@
+const circles = document.querySelectorAll('.rating_circle')
+const rating = document.querySelector('.rating_choice')
+const submit = document.querySelector('.submit')
+
+
+
+//Change Page
+function pag2click() {
+    window.location.href = './pags/pag2.html'   
+}
+
+//Identify Button
+function identifyClickedButton(event) {
+    const buttonClick = event.target.textContent;
+    //Stores the button that was clicked
+    localStorage.setItem("buttonClick", buttonClick);
+}
+
+circles.forEach(function(circles) {
+    circles.addEventListener("click", identifyClickedButton);
+})
+
+//Retrieves the clicked button that was stored
+const buttonClick = localStorage.getItem("buttonClick")
+    if (buttonClick) {
+        rating.innerHTML = `You selected ${buttonClick} out of 5`
+    }
+
+    submit.addEventListener('click', function() {
+        pag2click();
+    })
